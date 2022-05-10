6f6ce1eac6857556e0afcfe671ee93a000ce85cc
Renato <renatopdamazio@gmail.com>
2022-05-10 18:37:35 -0300

    Search input with decounce code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,37 @@
+import { useState } from "react";
+import { DebounceInput } from "react-debounce-input";
+const classNames = `
+rounded-md 
+w-full h-20 
+text-black 
+font-bold 
+text-3xl 
+text-center 
+uppercase 
+border-2
+border-gray
+shadow-inner
+transition-all
+focus:border-orange
+focus:shadow-orange
+focus:shadow-md
+shadow-sm
+px-6`;
 
 export default function Home() {
-  const classNames = `
-      rounded-md 
-      w-full h-20 
-      text-black 
-      font-bold 
-      text-3xl 
-      text-center 
-      uppercase 
-      border-2
-      border-gray
-      shadow-inner
-      transition-all
-      focus:border-orange
-      focus:shadow-orange
-      focus:shadow-md
-      shadow-sm
-      px-6`;
+  const searchArtist = (query: string) => {
+    console.log(query);
+  };
   return (
     <div className="flex p-10 align-center justify-center h-screen">
-      <input type="search" placeholder="O que gostaria de ouvir ?" autoFocus className={classNames} />
+      <DebounceInput
+        type="search"
+        placeholder="O que gostaria de ouvir ?"
+        autoFocus
+        debounceTimeout={300}
+        onChange={(ev) => searchArtist(ev.target.value)}
+        className={classNames}
+      />
     </div>
   );
 }
