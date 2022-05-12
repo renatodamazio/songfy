731f406ad0935f68975d1785aec9de04c7e0495a
Renato <renatopdamazio@gmail.com>
2022-05-12 15:55:47 -0300

    Slider smooth transition

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,161 +1,93 @@
 import React, { useEffect, useState, useRef } from "react";
 import Vinyl from "../components/Vinyl";
 import Image from "../components/Image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 
 export default function Carousel(props: any) {
-  const [items, setItems] = useState<any>([...props.items]);
-  const [start, _setStart] = useState<number>(0);
-  const [pressed, _setPressed] = useState<boolean>(false);
-  const [centerItem, _setCenterItem] = useState<any>();
+  const [items, {}] = useState<any>([...props.items]);
   const carousel: any = useRef(null);
-  const carouselInner: any = useRef(null);
-
-  const pressedRef: any = useRef(pressed);
-  const startRef: any = useRef(start);
-  const centerItemRef: any = useRef(centerItem);
-
-  const setPressed = (state: boolean) => {
-    pressedRef.current = state;
-    _setPressed(state);
-  };
-
-  const setStart = (state: number) => {
-    startRef.current = state;
-    _setStart(state);
-  };
-
-  const setCenterItem = (state: any) => {
-    centerItemRef.current = state;
-    _setCenterItem(state);
-  };
 
   const getAlbumImage = (data: any[]) => {
     const largeImage = data[3];
     return largeImage["#text"];
   };
 
-  const middleItem = (): any => {
-    const total: number = items.length / 2;
-    return Math.floor(total);
-  };
-
-  const Item = (data: any) => {
-    const { name, indice } = data.item;
+  const Item = (props: any) => {
+    const { name, image } = props;
     return (
-      <li className="h-60 flex justify-center align-middle content-center border-2">
-        {name}
-      </li>
+      <div className="carousel-item-wrapper">
+        <div className="carousel-item">{name}</div>
+      </div>
     );
   };
 
-  const carouselMouseDown = () => {
-    carousel.current.addEventListener("mousedown", (e: any) => {
-      e.preventDefault();
+  const configureSlides = (props: any) => {
+    const { slides } = props;
+    if (!slides.length) return;
+    let index: number = 0;
+    let onTop: any = [];
+    let onBottom: any = [];
 
-      const distance: number = Number(
-        Math.floor(e.offsetY - carouselInner.current.offsetTop)
-      );
-      const startMouseMove: number = distance;
-      setStart(startMouseMove);
-
-      carousel.current.style.cursor = "grabbing";
-
-      setPressed(true);
+    slides.map((item: any, key: number) => {
+      if (item.classList.contains("swiper-slide-active")) return (index = key);
     });
-  };
 
-  const carouselMouseEnter = () => {
-    carousel.current.addEventListener("mouseenter", (e: any) => {
-      carousel.current.style.cursor = "grab";
-    });
-  };
+    for (let y = slides.length - 1; y > index; y--) {
+      onBottom.push(slides[y]);
+    }
 
-  const carouselMouseUp = () => {
-    carousel.current.addEventListener("mouseup", (e: any) => {
-      setPressed(false);
-      carousel.current.style.cursor = "grab";
-    });
-  };
-
-  const carouselMove = () => {
-    carousel.current.addEventListener("mousemove", (e: any) => {
-      if (!pressedRef.current) return;
-
-      e.preventDefault();
-
-      let y = e.offsetY;
-      let startY = startRef.current;
-
-      carouselInner.current.style.top = `${y - startY}px`;
-
-      for (let i = items.length; i--; ) {
-        centerSliderItem(i);
-      }
-
-      checkBounderies();
-    });
-  };
-
-  const checkBounderies = () => {
-    let outer = carousel.current.getBoundingClientRect();
-    let inner = carouselInner.current.getBoundingClientRect();
-
-    let total = inner.top - outer.height;
-
-    if (parseInt(carouselInner.current.style.top) > 0) {
-      try {
-        carouselInner.current.style.top = "0px";
-      } catch (err) {
-        console.log("error...");
-      }
-    } else if (Math.abs(total) > inner.height) {
-      try {
-        carouselInner.current.style.top = `-${inner.height - outer.height}px`;
-      } catch (err) {
-        console.log("error...");
+    for (let x = index ; x >= 0; x--) {
+      if (!slides[x].classList.contains("swiper-slide-next")) {
+         onTop.push(slides[x]);
       }
     }
+
+    onTop.forEach((item: any) => {
+      const element = item.querySelectorAll(".carousel-item")[0];
+      element.classList.add("on-top");
+      element.classList.remove("on-bottom");
+    });
+
+    onBottom.forEach((item: any) => {
+      const element = item.querySelectorAll(".carousel-item")[0];
+      element.classList.add("on-bottom");
+      element.classList.remove("on-top");
+    });
+
   };
-
-  const centerSliderItem = (index: number) => {
-    try {
-      const percZone = 10;
-      const element =
-        carousel.current.querySelectorAll(".carousel-inner li")[index];
-      const pos = element.getBoundingClientRect();
-      const topPerc = (pos.top / carouselInner.current.offsetHeight) * 100;
-      const bottomPerc =
-        (pos.bottom / carouselInner.current.offsetHeight) * 100;
-      
-        const middle = Math.floor(topPerc + bottomPerc) / 2;
-      const inViewPort = middle > percZone && middle < 27 - percZone;
-
-    
-      element.classList.toggle("carousel-center-item", inViewPort);
-    } catch (err) {}
-  };
-
-  useEffect(() => {
-    const mid = middleItem();
-
-    carouselMouseDown();
-    carouselMouseEnter();
-    carouselMouseUp();
-    carouselMove();
-
-    checkBounderies();
-
-    centerSliderItem(mid);
-  }, []);
 
   return (
     <div className="carousel" ref={carousel}>
-      {pressed ? "sim" : "Não"}
-      <ul className="carousel-inner" ref={carouselInner}>
+      <Swiper
+        mousewheel={true}
+        spaceBetween={0}
+        slidesPerView="auto"
+        freeMode={false}
+        slideToClickedSlide={true}
+        freeModeSticky={true}
+        effect="coverflow"
+        centeredSlides={true}
+        direction="vertical"
+        coverflowEffect={{
+          rotate: -5,
+          stretch: 270,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        onSlideChangeTransitionEnd={(swiper: any) => configureSlides(swiper)}
+        onSwiper={(swiper: any) => configureSlides(swiper)}
+      >
         {items.map((item: any, key: number) => {
-          return <Item key={key} indice={key} item={item} />;
+          return (
+            <SwiperSlide key={key}>
+              <Item name={item.name} />
+            </SwiperSlide>
+          );
         })}
-      </ul>
+      </Swiper>
     </div>
   );
 }
