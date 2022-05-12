366051f79d6a226d17dcdae187625c20f487e9bf
Renato <renatopdamazio@gmail.com>
2022-05-11 23:28:12 -0300

    Dragging moviment working

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,11 +4,24 @@ import Image from "../components/Image";
 
 export default function Carousel(props: any) {
   const [items, setItems] = useState<any>([...props.items]);
-  const [start, setStart] = useState<number>(0);
-  const [pressed, setPressed] = useState<boolean>(false);
+  const [start, _setStart] = useState<number>(0);
+  const [pressed, _setPressed] = useState<boolean>(false);
 
   const carousel: any = useRef(null);
   const carouselInner: any = useRef(null);
+
+  const pressedRef: any = useRef(pressed);
+  const startRef: any = useRef(start);
+
+  const setPressed = (state: boolean) => {
+    pressedRef.current = state;
+    _setPressed(state);
+  };
+
+  const setStart = (state: number) => {
+    startRef.current = state;
+    _setStart(state);
+  };
 
   const getAlbumImage = (data: any[]) => {
     const largeImage = data[3];
@@ -27,12 +40,16 @@ export default function Carousel(props: any) {
   const carouselMouseDown = () => {
     carousel.current.addEventListener("mousedown", (e: any) => {
       e.preventDefault();
-      setPressed(true);
 
-      const startMouseMove = e.offsetX - carouselInner.current.offsetTop;
+      const distance: number = Number(
+        Math.floor(e.offsetY - carouselInner.current.offsetTop)
+      );
+      const startMouseMove: number = distance;
       setStart(startMouseMove);
 
       carousel.current.style.cursor = "grabbing";
+
+      setPressed(true);
     });
   };
 
@@ -44,7 +61,23 @@ export default function Carousel(props: any) {
 
   const carouselMouseUp = () => {
     carousel.current.addEventListener("mouseup", (e: any) => {
+      setPressed(false);
       carousel.current.style.cursor = "grab";
+    });
+  };
+
+  const carouselMove = () => {
+    carousel.current.addEventListener("mousemove", (e: any) => {
+      if (!pressedRef.current) return;
+
+      e.preventDefault();
+
+      let y = e.offsetY;
+      let startY = startRef.current;
+
+      console.log(startY);
+
+      carouselInner.current.style.top = `${y - startY}px`;
     });
   };
 
@@ -52,10 +85,13 @@ export default function Carousel(props: any) {
     carouselMouseDown();
     carouselMouseEnter();
     carouselMouseUp();
+
+    carouselMove();
   }, []);
 
   return (
     <div className="carousel" ref={carousel}>
+      {pressed ? "sim" : "Não"}
       <ul className="carousel-inner" ref={carouselInner}>
         {items.map((item: any, key: number) => {
           return <Item key={key} indice={key} item={item} />;
