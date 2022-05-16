0c66b742a01ef65af6d86d6a969dd6f341a7aac3
Renato <renatopdamazio@gmail.com>
2022-05-16 18:38:04 -0300

    vinyl table sport

--- src/components/Turntable/Needle.tsx
+++ src/components/Turntable/Needle.tsx
@@ -8,12 +8,12 @@ export default function Needle() {
     <>
       <div className="needle-wrapper">
         <i className="handle-needle"></i>
-        <span className="box-front">1</span>
-        <span className="box-back">2</span>
-        <span className="box-left">3</span>
-        <span className="box-right">4</span>
-        <span className="box-top">5</span>
-        <span className="box-bottom">6</span>
+        <span className="box-front"></span>
+        <span className="box-back"></span>
+        <span className="box-left"></span>
+        <span className="box-right"></span>
+        <span className="box-top"></span>
+        <span className="box-bottom"></span>
         <i className="needle"></i>
       </div>
     </>
