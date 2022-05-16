0f481d6563077cd692441d531e6f428ca3ce5301
Renato <renatopdamazio@gmail.com>
2022-05-16 17:23:42 -0300

    Needle wrapper 3d

--- src/components/Turntable/Cube.tsx
+++ src/components/Turntable/Cube.tsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Vinyl from "../Vinyl";
 import Needle from "./Needle";
 
 import Baoard from "./Board";
 export default function Table(props: any) {
   const { rotate } = props;
+  const [totalRounds, {}] = useState<any>([1, 2]);
+  let inital: number = 60;
 
   return (
-    <div className={`${rotate ? "rotate-table" : ""} cube`}>
-      <div className="vinyl-on-table"><Vinyl/></div>
-      <div className="vinyl-base"></div>
-      <div className="board"></div>
-      <div className="wood"></div>
-      <div className="wood wood-lateral right"></div>
-      {/* <div className="wood"></div> */}
-      {/* <div className="back">BACK</div> */}
-      {/* <div className="front">FRONT</div>*/}
-      {/* <div className="right"></div> */}
-      {/* <div className="left">LEFT</div>  */}
-    </div>
+    <>
+      <div className="needle-wrapper">
+        <span className="box-front">1</span>
+        <span className="box-back">2</span>
+        <span className="box-left">3</span>
+        <span className="box-right">4</span>
+        <span className="box-top">5</span>
+        <span className="box-bottom">6</span>
+      </div>
+      <div className={`${rotate ? "rotate-table" : ""} cube`}>
+        <div className="vinyl-on-table">
+          <Vinyl />
+        </div>
+        <div className="vinyl-base"></div>
+        <div className="arm">
+          <div className="stick-round-top">
+            {totalRounds.map((item: number) => {
+              return <span key={item} className={`spot-${item}`}></span>;
+            })}
+          </div>
+          <div className="stick-long"></div>
+        </div>
+        <div className="board"></div>
+        <div className="wood"></div>
+        <div className="wood wood-lateral right"></div>
+        {/* <div className="wood"></div> */}
+        {/* <div className="back">BACK</div> */}
+        {/* <div className="front">FRONT</div>*/}
+        {/* <div className="right"></div> */}
+        {/* <div className="left">LEFT</div>  */}
+      </div>
+    </>
   );
 }
