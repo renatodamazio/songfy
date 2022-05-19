f5cb6517d7e4225037e64d38bce6d0414cc226f9
Renato <renatopdamazio@gmail.com>
2022-05-19 17:24:52 -0300

    Arm css

--- src/components/Turntable/Cube.tsx
+++ src/components/Turntable/Cube.tsx
@@ -5,10 +5,6 @@ import Cylinder from "../Cylinder";
 export default function Table(props: any) {
   const { rotate } = props;
   const [totalRounds, {}] = useState<any>([1, 2]);
-  const [pieces, {}] = useState<any>([1, 2, 3, 4, 5, 6, 7, 8]);
-  const [piecesPool, {}] = useState<any>([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-  ]);
 
   return (
     <>
@@ -47,8 +43,25 @@ export default function Table(props: any) {
           </div>
         </div>
         <div className="board"></div>
-        <div className="wood"></div>
-        <div className="wood wood-lateral right"></div>
+        <div className="wood-wrapper">
+          <div className="wood wood-front"></div>
+          <div className="wood wood-left"></div>
+          <div className="wood wood-right"></div>
+          <div className="wood wood-back"></div>
+          <div className="wood wood-bottom">
+            <span className="about-author">
+              <ul>
+                <li>Renato Damázio</li>
+                <li>
+                  <a href="https://github.com/renatodamazio" target={"_blank"} rel="noreferrer noopener">Github</a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/renato-dam%C3%A1zio/" target={"_blank"} rel="noreferrer noopener">Linkedin</a>
+                </li>
+              </ul>
+            </span>
+          </div>
+        </div>
       </div>
     </>
   );
