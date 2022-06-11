ca81f8010f53fd94a66cd6dc8ace08ab4e5023f9
Renato <renatopdamazio@gmail.com>
2022-06-11 11:32:59 -0300

    Slider bottom with albums

--- src/components/AlbumsList.tsx
+++ src/components/AlbumsList.tsx
@@ -3,20 +3,54 @@ import getImageFromAPi from "../utils/getImageFromApi";
 import { Link } from "react-router-dom";
 import { setOpen } from "../store/reducers/albumOpenReducer";
 import { useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper";
+import { useLocation } from "react-router-dom";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { useEffect } from "react";
 
 export default function AlbumsList(props: any) {
+  let { search } = useLocation();
   const { albums } = props;
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (typeof albums.album === "object") {
+      const albumName = search.replace("?album=", "").replace(/\+/gi, " ");
+      const paginations: any = document.querySelectorAll(
+        ".swiper-pagination-bullet"
+      );
+      let page = 0;
+
+      albums?.album?.forEach((item: any, key: number) => {
+        if (item.name === albumName) page = key;
+      });
+
+      paginations[page]?.click();
+    }
+  }, [search]);
+
   return (
-    <div className="glass-morphism h-full absolute top-0 left-0 z-20 text-white opacity-5 hover:opacity-100 transition-ease-in-renato px-4">
-      <ul className="overflow-y-auto h-full">
+    <div className="album-list glass-morphism">
+      <Swiper
+        slidesPerView={9}
+        spaceBetween={30}
+        freeMode={true}
+        slideToClickedSlide={true}
+        pagination={{
+          clickable: true,
+        }}
+        centeredSlides={true}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
         {albums.album &&
           albums?.album?.map((item: any) => {
             return (
-              <li
-                key={item.name}
-                className="p-2 transition-ease-in-renato hover:scale-105"
-              >
+              <SwiperSlide key={item.name} className="hover:scale-105">
                 <Link to={"#!"} onClick={() => dispatch(setOpen(item))}>
                   <Image
                     className="rounded-lg"
@@ -24,10 +58,10 @@ export default function AlbumsList(props: any) {
                     src={getImageFromAPi({ images: item.image, size: 2 })}
                   />
                 </Link>
-              </li>
+              </SwiperSlide>
             );
           })}
-      </ul>
+      </Swiper>
     </div>
   );
 }
